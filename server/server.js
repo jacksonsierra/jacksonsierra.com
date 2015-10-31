72500d7dbe93bfbe72b301cf67322a722608928d
Jackson Sierra <jks718@gmail.com>
2015-10-30 18:12:40 -0700

    [setup] (server) add Express app in server.js

---
+++ server/server.js
@@ -0,0 +1,27 @@
+/*
+    file: server.js
+    - - - - - - - - - - - 
+    Express app that handles
+    routing for jacksonsierra.com
+ */
+var http = require('http')
+  , https = require('https')
+  , express = require('express')
+  , url = require('url')
+  , bodyParser = require('body-parser');
+
+var app = express();
+var server = http.createServer(app);
+  // , serverSSL = https.createServer(app);
+
+// app.use(express.static(__dirname + '/client/dist');
+app.use(express.static('client'));
+app.set('title', 'Jackson Sierra');
+
+app.get('/', function(req, res) {
+  res.end();
+});
+
+server.listen(8000, 'localhost', function() {
+  console.log('Listening on localhost:8000');
+});
