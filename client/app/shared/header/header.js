1a2d6df309761e5bd040b8525ff482374bf20001
Jackson Sierra <jks718@gmail.com>
2015-11-09 14:54:17 -0800

    [fix] (client) correct resolution issue with hamburger functionality

--- client/app/shared/header/header.js
+++ client/app/shared/header/header.js
@@ -4,4 +4,16 @@ angular.module('jacksonSierra.header', [])
 
 .controller('HeaderCtrl', ['$scope', function($scope) {
   $scope.hamburgerClicked = false;
+
+  $scope.toggleMenu = function() {
+    $scope.hamburgerClicked = !$scope.hamburgerClicked;
+
+    if($scope.hamburgerClicked) {
+      angular.element('.navibar').css('display', 'block');
+      angular.element('.logo').css('margin-bottom', '15px')
+    } else {
+      angular.element('.navibar').css('display', 'none');
+      angular.element('.logo').css('margin-bottom', '0px')
+    }
+  }
 }]);
