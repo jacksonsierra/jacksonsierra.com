1d0111cec6761dcc15ac196cfdd7c2125a918fe2
Jackson Sierra <jks718@gmail.com>
2015-12-08 11:02:45 -0500

    [feature] (client) implement carousel on homepage

--- client/app/components/home/home.js
+++ client/app/components/home/home.js
@@ -3,5 +3,20 @@
 angular.module('jacksonSierra.home', [])
 
 .controller('HomeCtrl', ['$scope', function($scope) {
-  
+  $scope.slideInterval = 5000;
+  $scope.noWrapSlides = false;
+  $scope.slideArray = [
+      {
+          url: '../img/botswana-color.min.jpg'
+        , caption: 'OKAVANGO DELTA, BOTSWANA'
+      }
+    , {
+          url: '../img/belize.min.jpg'
+        , caption: 'PLACENCIA, BELIZE'
+      }
+    , {
+          url: '../img/johnsville.min.jpg'
+        , caption: 'JOHNSVILLE, CALIFORNIA'
+      }
+  ];
 }]);
