3c561db8d81401cfb3ea24611a61fdfcb918144d
Jackson Sierra <jks718@gmail.com>
2015-12-15 18:28:49 -0500

    [refactor] (client) remove extraneous components inherited from Angular seed project

--- client/app/components/version/version.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('jacksonSierra.version', [
-  'jacksonSierra.version.interpolate-filter',
-  'jacksonSierra.version.version-directive'
-])
-
-.value('version', '0.1');
