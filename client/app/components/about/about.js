b3d0b16aa9254b62ee017e7628be162dc944d6b1
Jackson Sierra <jks718@gmail.com>
2015-12-17 21:32:08 -0800

    [refactor] (client) move About page description to About model

--- client/app/components/about/about.js
+++ client/app/components/about/about.js
@@ -1,7 +1,11 @@
 'use strict';
 
-angular.module('jacksonSierra.about', [])
+angular.module('jacksonSierra.about', ['ngSanitize'])
 
-.controller('AboutCtrl', [function() {
-
+.controller('AboutCtrl', ['$scope', '$sce', function($scope, $sce) {
+  $scope.descrip = $sce.trustAsHtml('<strong>Jackson Sierra</strong> is a software engineer and thought partner interested in tackling civic and socioeconomic problems through the use of technology.<br>
+                    <br>
+                    His studies in economics in the heart of Silicon Valley saw him pursue a career in the technology industry at a startup where he did strategy and data analysis.<br>
+                    <br>
+                    His time there ultimately showed him the incredible impact one can have building technology that enables the masses.  As such, he was driven to learn computer programming to accomplish that goal.');
 }]);
