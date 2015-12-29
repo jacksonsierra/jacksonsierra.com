7302d2a99302ff587173210cafde0801b04064f1
Jackson Sierra <jks718@gmail.com>
2015-12-29 13:20:34 -0800

    [refactor] (server) incorporate image middleware into server.js

--- server/server.js
+++ server/server.js
@@ -5,21 +5,24 @@
     routing for jacksonsierra.com
  */
 var http = require('http')
-  , https = require('https')
+  // , https = require('https')
   , express = require('express')
-  , url = require('url')
-  , bodyParser = require('body-parser');
+  , bodyParser = require('body-parser')
+  , images = require('./db/images.js');
 
 var app = express();
 var server = http.createServer(app);
   // , serverSSL = https.createServer(app);
 
+app.use(bodyParser.json());
 app.use(express.static('./client/dist'));
 app.set('title', 'Jackson Sierra');
 
-app.get('/*', function(req, res) {
+app.get('/^(?!\/api\/)[\\s\\S]*/', function(req, res) {
   res.sendFile('index.html', {root: './client/dist'});
 });
+
+app.get('/api/images*', images.findImages);
 
 server.listen(8000, 'localhost', function() {
   console.log('Listening on localhost:8000');
