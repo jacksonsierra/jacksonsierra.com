7e8fe3f210973ef164e05b8e8f233749cd1fdf0c
Jackson Sierra <jks718@gmail.com>
2015-12-29 13:20:14 -0800

    [feature] (server) add middleware for site images

---
+++ server/db/images.js
@@ -0,0 +1,21 @@
+var db = require('./db.js')
+  , url = require('url');
+
+module.exports = {
+  findImages: function(req, res) {
+    var page = url.parse(req.originalUrl, true).query.page
+      , query = {
+          'active': true
+        };
+
+    if(page !== 'all') query.page = page;
+
+    db.images.find(query).sort({'order': 1}).toArray(function(err, docs) {
+      if(err) res.status(404).send(err);
+
+      res.status(200).send(docs);
+      return;
+    });
+    return;
+  }
+};
