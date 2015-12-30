772be6c60df9aeead840eea255703c0f53656a76
Jackson Sierra <jks718@gmail.com>
2015-12-30 14:03:40 -0800

    [feature] (server) add middleware for site blog posts

---
+++ server/db/blog_posts.js
@@ -0,0 +1,29 @@
+var db = require('./db.js')
+  , url = require('url')
+  , moment = require('moment');
+
+var cleanDate = function(date) {
+  return moment(date).format('dddd, MMM Do YYYY');
+};
+
+module.exports = {
+  findBlogPosts: function(req, res) {
+    var query = {};
+
+    db.blog_posts.find(query).toArray(function(err, docs) {
+      if(err) res.status(404).send(err);
+
+      docs.forEach(function(element) {
+        var dateString = element.date;
+
+        element.date = new Date(dateString);
+        element.dateFormatted = cleanDate(dateString);
+        return;
+      });
+
+      res.status(200).send(docs);
+      return;
+    });
+    return;
+  }
+};
