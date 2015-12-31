b3d9fac8bac98a586395eb9ea6181f70cbcbd1b7
Jackson Sierra <jks718@gmail.com>
2015-12-30 19:30:35 -0800

    [refactor] (client) configure about component to work with factory

--- client/app/components/about/about.js
+++ client/app/components/about/about.js
@@ -1,11 +1,7 @@
 'use strict';
 
-angular.module('jacksonSierra.about', ['ngSanitize'])
+angular.module('jacksonSierra.about', [])
 
-.controller('AboutCtrl', ['$scope', '$sce', function($scope, $sce) {
-  $scope.descrip = $sce.trustAsHtml('<strong>Jackson Sierra</strong> is a software engineer and thought partner interested in tackling civic and socioeconomic problems through the use of technology.<br>
-                    <br>
-                    His studies in economics in the heart of Silicon Valley saw him pursue a career in the technology industry at a startup where he did strategy and data analysis.<br>
-                    <br>
-                    His time there ultimately showed him the incredible impact one can have building technology that enables the masses.  As such, he was driven to learn computer programming to accomplish that goal.');
+.controller('AboutCtrl', ['$scope', 'Images', function($scope, Images) {
+  $scope.images = Images.query({page: 'about'});
 }]);
